1d021ad6dfb9c2942b4431a89cc4d849a6d7eca0
Priyam Joshi <priyamj249@gmail.com>
2026-05-17 16:58:12 +0530

    Fix CORS for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,9 +11,11 @@ const app = express();
 
 // Security Middleware
 app.use(helmet());
+
+// Allow requests from anywhere (we can lock this down to just Vercel later)
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', process.env.ADMIN_URL || 'http://localhost:5173'],
-  credentials: true
+    origin: '*',
+    credentials: true
 }));
 
 // Body Parsing
